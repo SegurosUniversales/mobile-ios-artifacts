e8c85b474205bc712769c4c8dc1af1f2da6d5f97
jculajayuniversales <jculajay@universales.com>
2023-09-01 09:18:09 -0600

    prueba de lectura app.json
    prueba de lectura app.json

---
+++ appLanding.js
@@ -0,0 +1,19 @@
+fetch('./app.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let ul = document.getElementById("apps-list");
+
+        console.log(json);
+        for (const app in json) {
+            if (Object.hasOwnProperty.call(json, app)) {
+                const element = json[app];
+                let li = `<li>
+                <a href="itms-services://?action=download-manifest&url=https://apps.universales.com/ajustadores/${element.version}/manifest.plist">
+                    <img src="imgs/logoApp.svg" alt="${app}"> Ajustadores
+                    <span>${element.package}</span>
+                </a>
+            </li>`;
+                ul.innerHTML += li.trim();   
+            }
+        }
+});
