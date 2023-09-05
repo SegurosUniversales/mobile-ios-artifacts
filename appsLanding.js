d4e73b0294e4ad001dbeaa2c4af3ccd12d408d5e
jculajayuniversales <jculajay@universales.com>
2023-09-05 16:31:46 -0600

    Update appsLanding.js

--- appsLanding.js
+++ appsLanding.js
@@ -6,16 +6,12 @@ fetch('./app.json')
         for (const app in json) {
             if (Object.hasOwnProperty.call(json, app)) {
                 const element = json[app];
-                let li = "";
-                switch(app){
-                    case "ajustadores":
-                         li = `<li>
-                                    <a href="itms-services://?action=download-manifest&url=https://apps.universales.com/Ajustadores/${element.version}/manifest.plist" title="v: ${element.version}">
-                                        <img src="imgs/logoApp.svg" alt="${app}"> 
-                                       Ajustadores
-                                    </a>
-                                 </li> `;   
-                }
+                let li = li = `<li>
+                <a href="itms-services://?action=download-manifest&url=https://apps.universales.com/${app}/${element.version}/manifest.plist" title="v: ${element.version}">
+                    <img src="imgs/logoApp.svg" alt="${app}"> 
+                   Ajustadores
+                </a>
+             </li> `; 
                 
                 ulIos.innerHTML += li.trim();   
             }
