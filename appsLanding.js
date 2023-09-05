1a79611e98f993b7f951528862d7c0aecb86d36f
dmhernandez-su <84216157+dmhernandez-su@users.noreply.github.com>
2023-09-05 15:04:50 -0600

    Initial commit

---
+++ appsLanding.js
@@ -0,0 +1,23 @@
+fetch('./app.json')
+    .then((response) => response.json())
+    .then((json) => {
+        let ulIos = document.getElementById("ios-apps-list-container");
+        
+        for (const app in json) {
+            if (Object.hasOwnProperty.call(json, app)) {
+                const element = json[app];
+                let li = "";
+                switch(app){
+                    case "ajustadores":
+                         li = `<li>
+                                    <a href="itms-services://?action=download-manifest&url=https://apps.universales.com/Ajustadores/${element.version}/manifest.plist" title="v: ${element.version}">
+                                        <img src="imgs/logoApp.svg" alt="${app}"> 
+                                       Ajustadores
+                                    </a>
+                                 </li> `;   
+                }
+                
+                ulIos.innerHTML += li.trim();   
+            }
+        }
+});
